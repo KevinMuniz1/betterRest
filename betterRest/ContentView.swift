7624a306528cfa6b6a69a8800e89ce1ce497d5c7
KevinMuniz1 <munizkevin@outlook.com>
2024-01-10 12:31:36 -0500

    Initial Commit

---
+++ betterRest/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  betterRest
+//
+//  Created by Kevin Muniz on 1/10/24.
+//
+
+import SwiftUI
+
+
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ betterRest/betterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  betterRestApp.swift
+//  betterRest
+//
+//  Created by Kevin Muniz on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct betterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
