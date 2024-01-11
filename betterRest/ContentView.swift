148d556fe825ff1ac5c9fcf06e87f12ed03d4922
KevinMuniz1 <munizkevin@outlook.com>
2024-01-11 15:23:49 -0500

    layed out the structure for the app by using a date picker and two steppers so the user can easily increment and decrement a value depending on what value they want to use

--- betterRest/ContentView.swift
+++ betterRest/ContentView.swift
@@ -10,10 +10,31 @@ import SwiftUI
 
 
 struct ContentView: View {
+    @State private var wakeup = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var cupsOfCoffeeHad = 0
     var body: some View {
-        VStack {
-            Text("")
-        }
+        NavigationStack {
+            VStack() {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker("Please select a time", selection: $wakeup, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                Text("Coffee amount:")
+                    .font(.headline)
+                Stepper("\(cupsOfCoffeeHad) cups of coffee", value: $cupsOfCoffeeHad, in: 0...20)
+            }
+            .toolbar {
+                Button("Calculate", action: calculateSleep)
+            }
+            .navigationTitle("BetterRest")
+        }.padding()
+    }
+    func calculateSleep() {
+        
     }
 }
 
